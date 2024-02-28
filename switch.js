a57ed55e5d175e6a45c2d1009e718214c0c7c85c
Goswamianshu12300 <150151522+Goswamianshu12300@users.noreply.github.com>
2024-02-28 15:50:25 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,195 @@
+// a=prompt("Enter the no.")
+// b=a%2
+// switch(b){
+//     case 0:
+//         document.write("even No")
+//         break;
+//         case 1:
+//         document.write("odd no")
+//         break;
+//         case 2:
+//         document.write("Valid no")
+//         break;
+// }
+// a=prompt("enter the character")
+// b("a,e,i,o,u,")
+// switch(b){
+//     case 0:
+//         document. write()
+//         case 1:
+//         document. write()
+//         case 1:
+//         document. write()
+
+
+// }
+// a=parseInt(prompt("enter the week name"))
+// switch(a){
+//     case 1:
+//         document.write("monday")
+//         break;
+//         case 2:
+//         document.write("tuesday")
+//         break;
+//         case 3:
+//         document.write("wednesday")
+//         break;
+//         case 4:
+//         document.write("thursday")
+//         break;
+//         case 5:
+//         document.write("friday")
+//         break;
+//         case 6:
+//         document.write("saturday")
+//         break;
+//         case 7:
+//             document.write("sunday")
+//             break;
+//             default:
+//                 document.write("valid no")
+//                     }
+//                     a=prompt("enter the fruits name")
+// switch(a){
+//     case "Mango":
+//         document.write("Yellow")
+//         break;
+//         case "banana":
+//         document.write("Yellow")
+//         break;
+//         case "papaya":
+//         document.write("Yellow")
+//         break;
+//         case "Apple":
+//         document.write("Red")
+//         break;
+//         case "Orange":
+//         document.write("Orange")
+//         break;
+// }
+// a=parseInt(prompt(" enter the months name"))
+// switch(a){
+//     case 1:
+//         document.write("January")
+//         break;
+//         case 2:
+//         document.write("February")
+//         break;
+//         case 3:
+//         document.write("March")
+//         break;
+//         case 4:
+//         document.write("April")
+//         break;
+//         case 5:
+//         document.write("May")
+//         break;
+//         case 6:
+//         document.write("June")
+//         break;
+//         case 7:
+//         document.write("July")
+//         break;
+//         case 8:
+//         document.write("August")
+//         break;
+//         case 9:
+//         document.write("September")
+//         break;
+//         case 10:
+//         document.write("Octuber")
+//         break;
+//         case 11:
+//         document.write("November")
+//         break;
+//         case 12:
+//         document.write("dEcember")
+//         break;
+// }
+// a=parseInt(prompt("enter the number"))
+// switch(a)
+// for(a=50;a<=100;++a)
+// document.write(a,"<br>")
+// for(b=1;b<=10;++b){
+// document.write("2 X",b,"=",b*2,"<br>")                                                 
+//     }
+
+
+     
+
+
+// for(a=1;a<100;++a)
+// if(a%4==0)
+// "document.write(a,<br>")  
+
+
+
+// a=parseInt(prompt("Enter the serving year"))
+// b=parseInt(prompt("Enter the salary"))
+// c=b+b*5/100
+// if(a>=5){
+//     document.write(c)
+// }
+// else{
+//     document.write(b)
+// }
+// a=parseInt(prompt("Enter the length"))
+// b=parseInt(prompt("enter the breadth"))
+// if(a==b){
+//     document.write("Square")
+// }
+// a=[23,34,56,78,25,89,95,68,70]
+// document.write(a. length)
+// a=[34,56,76,68,95,24]
+// sum=0
+// for(i=0;i<a.length;++i){
+//     sum=sum+a[i]
+// }
+// document.write(sum)
+// a=[23,34,56,78,25,89,95,68,70]
+// min=a[0]
+// for(i=0;i<a.length;++i){
+//     if(min<a[i]){
+//         min=a[i]
+//     }
+
+// }
+// document.write(min)
+// a=[23,34,56,78,25,89,95,68,70]
+// for(i=0;i<a.length;++i)
+// if(a[i]%2==1){
+//     document.write(a[i],'Is a odd number<br>')
+// }
+// a=[24,46,34,56,34,67,45,89]
+// ascending=0
+// for(i=0;i<a.length;++1){
+// ascending=ascending+a[i]
+// }
+// document.write(a.sort())
+// for(i=0;i<a.lenght;++i){
+
+// }
+// document.write(a[i],median)
+function abs(){
+    a=document.getElementById('aa')
+                      
+    if(a.style.display!='none'){
+        a.style.display='none'
+        document.body.style.background="pink"
+    }
+    else{
+        a.style.display='block'
+        a.style.background='aqua'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
